9498d927bb9ad1674993c7223a7ea0b5dc70dbc6
Savindi <savindipornima@gmail.com>
2026-03-08 20:10:32 +0530

    Bonus collaboration: added by Savindi

--- README.txt
+++ README.txt
@@ -1,1 +1,5 @@
 Business Application Development
+
+Activity - Git
+
+Collaborated with Savindi for bonus marks!
--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
 print("Hello, World! This is version 3.")
 print("Adding a simple calculation: 2 + 2 = ", 2 + 2)
+
+# Bonus collaboration by Savindi
+print("Hope you enjoy learning Python and Git!")
+print("\nHave a great day!")
